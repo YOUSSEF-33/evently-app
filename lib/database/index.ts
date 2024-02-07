e81bc14e2f0382d685f8928bcbf931806d72f9b0
Youssef Atef <yatef57@gmail.com>
2024-02-07 15:53:43 +0200

    fifth

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,15 +1,12 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 const cached = (global as any).mongoose || { conn: null, promise: null };
-const mongoUrl = (process.env.MONGODB_URL);
+const mongoUrl = process.env.MONGODB_URL;
 
 export const connectToDB = async () => {
   if (cached.conn) return cached.conn;
   if(!mongoUrl) throw new Error("faild to connect");
-  cached.promise = cached.promise || mongoose.connect(mongoUrl, {
-    dbName:"evently",
-    bufferCommands:false,
-  });
+  cached.promise = cached.promise || mongoose.connect(mongoUrl);
   cached.conn = await cached.promise;
   return cached.conn;
 };
